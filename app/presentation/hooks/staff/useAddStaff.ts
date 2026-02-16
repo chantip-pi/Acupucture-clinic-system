cddc0ffb6ac63ac6191be02ab54fb603d7694091
chantip-pi <chantip.pi@ku.th>
2026-02-16 23:56:46 +0700

    remove bcryptjs lib

--- app/presentation/hooks/staff/useAddStaff.ts
+++ app/presentation/hooks/staff/useAddStaff.ts
@@ -2,7 +2,6 @@ import { useState } from "react";
 import { addStaffUseCase } from "~/infrastructure/di/container";
 import { CreateStaffDTO } from "~/application/dtos/StaffDTO";
 import { BackendErrorService } from "~/domain/services/ErrorService";
-import bcrypt from "bcryptjs";
 
 export function useAddStaff() {
   const [loading, setLoading] = useState(false);
@@ -13,12 +12,9 @@ export function useAddStaff() {
     setError(null);
 
     try {
-      // Hash password before sending to the backend
-      const hashedPassword = await bcrypt.hash(dto.password, 10);
-
       await addStaffUseCase.execute({
         ...dto,
-        password: hashedPassword,
+        password: dto.password,
       });
       return { success: true };
     } catch (err) {
